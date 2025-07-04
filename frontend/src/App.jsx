ced1eb643cda6998b21644d782e5d4e5a5de861c
SinghArsh04 <arshdeepsingh260409@gmail.com>
2025-07-04 22:18:27 +0530

    psh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
 
+import { useEffect, useState } from "react";
+import { Route, Routes, useLocation, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
+ 
+import Navbar from "./components/common/Navbar"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar />
+      </div>
       hello
     </>
   )
